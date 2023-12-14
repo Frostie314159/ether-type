1e7dcd9f5283fcecf4beb35d29e025224241379b
Frostie314159 <iam.an.programmer@gmail.com>
2023-12-14 19:21:46 +0100

    Removed debug feature.

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,10 @@
 use macro_bits::serializable_enum;
 
 serializable_enum! {
-    #[cfg_attr(feature = "debug", derive(Debug))]
-    #[derive(Clone, Copy, PartialEq, Eq)]
+    #[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
     /// The EtherType as described in IEEE 802.3
     pub enum EtherType: u16 {
+        #[default]
         IPv4 => 0x0800,
         ARP => 0x0806,
         WoLAN => 0x0842,
